a9c52013c0b00cd370f2b24ea09c0472cf1275a7
Daniel <malpartidahuamanidanielsariff@gmail.com>
2025-09-07 12:27:06 -0500

    Agregado primera parte main

---
+++ src/main/java/com/mycompany/semana4herramientas/Conflictos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.semana4herramientas;
+
+/**
+ *
+ * @author bruno
+ */
+public class Conflictos {
+    public Conflictos(){
+        System.out.println("Saludos desde la clase conflictos");
+        
+    }
+}
